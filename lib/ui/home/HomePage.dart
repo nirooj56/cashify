587908b9d928bd8cf58127a220fb7bc1fb09d458
Bista Nirooj <nirooj56@gmail.com>
2019-03-21 23:18:37 +0545

    Front-end design started to make some sense. Hola!!

--- lib/ui/home/HomePage.dart
+++ lib/ui/home/HomePage.dart
@@ -4,6 +4,7 @@ import 'package:cashmoney/ui/home/PlanetList.dart';
 
 String value = "";
 TextEditingController textController = TextEditingController();
+Color myColor = Color(0xFF00C6FF);
 
 class HomePage extends StatelessWidget {
   @override
@@ -12,51 +13,7 @@ class HomePage extends StatelessWidget {
         body: new HomePageBody(),
         floatingActionButton: FloatingActionButton(
           onPressed: () {
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                // return object of type Dialog
-                return AlertDialog(
-                  shape: RoundedRectangleBorder(
-                      borderRadius:
-                          const BorderRadius.all(const Radius.circular(16.0))),
-                  title: new TextField(
-                    controller: textController,
-                    maxLength: 15,
-                    textInputAction: TextInputAction.done,
-                    onChanged: (text) {
-                      value = text;
-                    }, //onChanged is called whenever we add or delete something on Text Field
-                    decoration: InputDecoration(
-                      border: UnderlineInputBorder(),
-                      fillColor: Colors.black,
-//                      prefixIcon: Icon(Icons.business_center),
-                      prefix: CircularProgressIndicator(),
-                      labelText: 'Company Name',
-                    ),
-                  ),
-                  actions: <Widget>[
-                    // usually buttons at the bottom of the dialog
-                    new RaisedButton(
-                      child: new Text("Close"),
-                      textColor: Colors.white,
-                      color: Colors.red,
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                    new RaisedButton(
-                      child: new Text("Save"),
-                      textColor: Colors.white,
-                      color: Colors.blue,
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    )
-                  ],
-                );
-              },
-            );
+            _showDialog(context);
           },
           backgroundColor: Color(0xFFFFFFFF),
           foregroundColor: Color(0xFF8685E5),
@@ -81,4 +38,82 @@ class _HomePageBodyState extends State<HomePageBody> {
       ],
     );
   }
+}
+
+void _showDialog(context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(32.0))),
+          contentPadding: EdgeInsets.only(top: 10.0),
+          content: Container(
+            width: 300.0,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Text(
+                      "Add a new Company",
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Divider(
+                  color: Colors.grey,
+                  height: 4.0,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 30.0, right: 30.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Company Name",
+                      border: InputBorder.none,
+                    ),
+                    maxLength: 15,
+                    maxLines: 4,
+                  ),
+                ),
+                InkWell(
+                  child: Container(
+                      padding: EdgeInsets.only(top: 05.0, bottom: 5.0),
+                      decoration: BoxDecoration(
+                        color: myColor,
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(32.0),
+                            bottomRight: Radius.circular(32.0)),
+                      ),
+                      child: Row(
+                        children: <Widget>[
+                          FlatButton(
+                            padding: EdgeInsets.only(right: 50.0, left: 50.0),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: new Text("Cancel"),
+                            textColor: Colors.white,
+                          ),
+                          FlatButton(
+                            padding: EdgeInsets.only(right: 50.0, left: 50.0),
+                            onPressed: () {},
+                            child: new Text("Save"),
+                            textColor: Colors.white,
+                          ),
+                        ],
+                      )),
+                ),
+              ],
+            ),
+          ));
+    },
+  );
 }
