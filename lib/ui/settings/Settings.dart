ad38ab797ae4f2066c4ac83b4da45bc2e5099efc
Bista Nirooj <nirooj56@gmail.com>
2019-03-29 17:38:03 +0545

    Front-end design started to make some sense. Hola!!

---
+++ lib/ui/settings/Settings.dart
@@ -0,0 +1,24 @@
+import 'package:cashmoney/Theme.dart' as Theme;
+import 'package:flutter/material.dart';
+
+class Settings extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      padding: new EdgeInsets.only(
+          top: MediaQuery
+              .of(context)
+              .padding
+              .top
+      ),
+      child: new Row(
+        children: <Widget>[
+          new BackButton(
+              color: Theme.Colors.appBarIconColor
+          ),
+        ],
+      ),
+    );
+  }
+}
